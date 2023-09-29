0f7051857fd1051a0ddb056c1bb08cf8e9dfdd90
Max Pershin <mepershin@gmail.com>
2023-09-29 17:55:47 +0300

    init from webdav

---
+++ build/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERNAME=webdav_user
+PASSWORD=$(dd if=/dev/urandom of=/dev/stdout \
+              bs=16 count=1 status=none |  hexdump -ve '1/1 "%.2x"')
+
+htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
+
+echo "Username: $USERNAME"
+echo "Password: $PASSWORD"
+
+chown -R www-data:www-data /bucket/files
+nginx -g 'daemon off;'
